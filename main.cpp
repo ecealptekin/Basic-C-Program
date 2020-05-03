6e8bc674b4417cbca40601dc7216818af19fdffa
Ece Alptekin <48667985+ecealptekin@users.noreply.github.com>
2020-05-03 19:11:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by MacBook Pro on 3.05.2020.
+//  Copyright © 2020 MacBook Pro. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int a,b;
+
+    cout << "Please give a number as 1st variable: ";
+    cin >> a;
+    cout << "Please give a number as 2nd variable: ";
+    cin >> b;
+    cout << "The value of the 1st variable: " << b << "\n";
+    cout << "The value of the 2nd variable: " << a << "\n";
+    
+    return 0;
+}
